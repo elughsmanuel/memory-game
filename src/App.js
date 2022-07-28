746f7078bc6917f419bb6baf24d1c08f5fc78847
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-07-28 14:52:19 +0100

    Matching Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", matched:false },
+  { src: "/img/potion-1.png", matched:false },
+  { src: "/img/ring-1.png", matched:false },
+  { src: "/img/scroll-1.png", matched:false },
+  { src: "/img/shield-1.png", matched:false },
+  { src: "/img/sword-1.png", matched:false }
 ];
 
 function App() {
@@ -39,6 +39,16 @@ function App() {
       
       if(choiceOne.src === choiceTwo.src) {
         console.log('cards match')
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched:true}
+            }
+            else {
+              return card
+            }
+          })
+        })
         resetTurn()
       }
       else {
@@ -47,6 +57,8 @@ function App() {
       }
     }
   }, [choiceOne, choiceTwo])
+
+  console.log(cards)
 
   // reset choices and increase turn
   const resetTurn = () => {
