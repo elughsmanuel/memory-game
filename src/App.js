edc9ef3a2ba0085a802be17697033ddaf0951d96
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-07-28 14:37:11 +0100

    Comparing Cards Choices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 
@@ -30,7 +30,30 @@ function App() {
   // handleChoice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    
   };
+
+  // compare two cards
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      
+      if(choiceOne.src === choiceTwo.src) {
+        console.log('cards match')
+        resetTurn()
+      }
+      else {
+        console.log('cards do not match')
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  // reset choices and increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className="App">
